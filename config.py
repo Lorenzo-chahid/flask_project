f3fc37b1f6cd8e09052425974d190a16bdb7e656
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2020-04-15 14:51:41 +0200

    pushing to production

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ SECRET_KEY = 'H4S6%YI}r,w5#"jya3#<\x0c>gh'
 
 FB_APP_ID = 162644931682205
 
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
