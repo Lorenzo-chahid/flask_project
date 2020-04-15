d400320f4980d16adee78e4c480c522d3702dba4
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2020-04-15 15:23:08 +0200

    add id

--- config.py
+++ config.py
@@ -7,5 +7,7 @@ FB_APP_ID = 162644931682205
 if os.environ.get('DATABASE_URL') is None:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    FB_APP_ID = 162644931682205
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    FB_APP_ID = 3084275108271122
