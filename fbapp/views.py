531b82c2efdb42d39c5b4447df3e4280c21c969a
lorenzo chahid <lorenzochahid@MacBook-Pro-de-lorenzo.local>
2020-04-15 14:38:39 +0200

    launchingtoproduction

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,32 +1,58 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+
+
+
 
 app = Flask(__name__)
 
+app.config.from_object('config')
+from .utils import find_content, OpenGraphImage
+
+
+
+
 
 @app.route('/')
+@app.route('/index/')
 def index():
+    if 'img' in request.args:
+        img = request.args['img']
+        og_url = url_for('index', img=img, _external=True)
+        og_image = url_for('static', filename=img, _external=True)
+    else:
+        og_url = url_for('index', _external=True)
+        og_image = url_for('static', filename='tmp/sample.jpg', _external=True)
     description = """Toi, tu sais comment utiliser la console ! Jamais à court d'idées pour réaliser ton objectif,
      tu es déterminé-e et persévérant-e. Tes amis disent d'ailleurs volontiers que tu as du caractère et
       que tu ne te laisses pas marcher sur les pieds. Un peu hacker sur les bords, tu aimes trouver des solutions à tout problème.
        N'aurais-tu pas un petit problème d'autorité ? ;-)"""
+    page_title = "Le test ultime"
+    og_url = url_for('index', _external=True)
+    og_image = url_for('static', filename="tmp/sample.jpg")
+    og_description = "Découvre qui tu es vraiment en faisant le test ultime !"
     return render_template('index.html',
                             user_name="Lorenzo",
-                            user_image=url_for('static', filename="tmp/cover_111823112767411.jpg"),
-                            description=description)
+                            user_image=url_for('static', filename="img/profile.png"),
+                            description=description,
+                            blur =True,
+                            page_title=page_title,
+                            og_url=og_url,
+                            og_image=og_image,
+                            og_description=og_description)
 
 
 @app.route('/result/')
 def result():
-    description = """Toi, tu n'as pas peur d'être seul ! Les grands espaces et les aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton émission préférée !
-     Bientôt tu partiras les cheveux au vent sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle chance !"""
+    gender = request.args.get('gender')
+    user_name = request.args.get('first_name')
+    uid = request.args.get('id')
+    profile_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
+    description= find_content(gender).description
+    img= OpenGraphImage(uid, user_name, description).location
+    og_url = url_for('index', img=img, _external=True)
     return render_template('result.html',
-                            user_name="Lorenzo",
-                            user_image=url_for('static', filename="tmp/cover_111823112767411.jpg"),
-                            description=description)
+                            user_name=user_name,
+                            user_image=profile_pic,
+                            description=description,
+                            og_url=og_url,)
 
-
-if __name__ == "__main__":
-    app.run()
-
-
-app.config.from_object('config')
